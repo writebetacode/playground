0f36aa1fddadeb415a4eea3f61432855bb1d5c01
writebetacode <scholle.mv@protonmail.com>
2021-01-19 11:10:42 -0500

    adding in some stupid changes for now

--- solid/projects/app/components/App/App.jsx
+++ solid/projects/app/components/App/App.jsx
@@ -2,36 +2,30 @@ import { lazy } from "solid-js";
 import {
   Link,
   MatchRoute,
-  NavLink,
   pathIntegration,
-  Router
+  Router,
+  useRouter
 } from "@rturnq/solid-router";
 import { AppChooser } from "../AppChooser/AppChooser";
 import "./App.css";
 
-const TicTacToe = lazy(
-  () => import("../../../tictactoe/components/TicTacToe/TicTacToe")
-);
+const TicTacToe = lazy(() =>
+  import("../../../tictactoe/components/TicTacToe/TicTacToe"));
+
+const apps = [
+  {
+    path: "tictactoe",
+    name: "Tic Tac Toe",
+    description: "simple game of tic tac toe",
+  }
+];
 
 export const App = () => {
-  const apps = [
-    {
-      path: "tictactoe",
-      name: "Tic Tac Toe",
-      description: "simple game of tic tac toe",
-      Component: TicTacToe
-    }
-  ];
-
   return (
     <Router integration={pathIntegration()}>
       <header>
-        <nav class="header">
-          <Link class="link default" href="/">Home</Link>
-          <For each={apps}>{app =>
-            <NavLink class="link" href={`/${app.path}`}>{app.name}</NavLink>
-          }</For>
-        </nav>
+        <Link class="link" href="/">Apps</Link>
+        <span class="name">Page Name Goes Here</span>
       </header>
       <main>
         <Switch fallback={<AppChooser apps={apps} />}>
