44753391cc7a4bb604a1216d1e6af4fbd686e2dc
Michael Scholle <5696777+writebetacode@users.noreply.github.com>
2021-01-29 14:09:29 -0500

    cleaning up Header.jsx headerClasses memo

--- solid/projects/tictactoe/components/Header/Header.jsx
+++ solid/projects/tictactoe/components/Header/Header.jsx
@@ -4,27 +4,23 @@ import { BOARD_V_DRAW, BOARD_A_RESET } from "../../store/board";
 import "./Header.css";
 
 export const Header = () => {
-  const [state, dispatch] = useStoreon();
+  const [state, dispatch] = useStoreon(),
+    playerText = createMemo(() => {
+      if (state.board.winner === "") {
+        return `${state.board.currentPlayer}'s move`;
+      }
 
-  const playerText = createMemo(() => {
-    if (state.board.winner === "") {
-      return `${state.board.currentPlayer}'s move`;
-    }
+      if (state.board.winner === BOARD_V_DRAW) {
+        return "Draw!";
+      }
 
-    if (state.board.winner === BOARD_V_DRAW) {
-      return "Draw!";
-    }
-
-    return `${state.board.winner} is the Winner!`;
-  }),
-  headerClasses = createMemo(() => {
-    let classes = ["header"]
-    if (state.board.winner.length) {
-      classes = [...classes, "winner-found"];
-    }
-    return classes;
-  }),
-  resetBoard = () => dispatch(BOARD_A_RESET);
+      return `${state.board.winner} is the Winner!`;
+    }),
+    headerClasses = createMemo(() => [
+      "header",
+      state.board.winner.length ? "winner-found" : "",
+    ]),
+    resetBoard = () => dispatch(BOARD_A_RESET);
 
   return (
     <div class={headerClasses().join(" ")}>
