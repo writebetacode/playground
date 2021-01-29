473f2c86f5320a1daf921ec2202c12b566b423fe
writebetacode <scholle.mv@protonmail.com>
2021-01-28 21:49:08 -0500

    adding in some better styles for the replay button and the playerText across a couple different screen sizes

--- solid/projects/tictactoe/components/Header/Header.jsx
+++ solid/projects/tictactoe/components/Header/Header.jsx
@@ -7,28 +7,31 @@ export const Header = () => {
   const [state, dispatch] = useStoreon();
 
   const playerText = createMemo(() => {
-    if(state.board.winner === "") {
+    if (state.board.winner === "") {
       return `${state.board.currentPlayer}'s move`;
     }
 
-    if(state.board.winner === BOARD_V_DRAW) {
+    if (state.board.winner === BOARD_V_DRAW) {
       return "Draw!";
     }
 
     return `${state.board.winner} is the Winner!`;
   }),
-  buttonClasses = createMemo(() => [
-    "replay-button",
-    state.board.winner === "" ? "hidden" : ""
-  ], state.board.winner),
+  headerClasses = createMemo(() => {
+    let classes = ["header"]
+    if (state.board.winner.length) {
+      classes = [...classes, "winner-found"];
+    }
+    return classes;
+  }),
   resetBoard = () => dispatch(BOARD_A_RESET);
 
   return (
-    <div class="header">
+    <div class={headerClasses().join(" ")}>
       <div class="player-text">
         {playerText()}
       </div>
-      <div class={buttonClasses().join(" ")}>
+      <div class="replay-button">
         <button type="button" onClick={resetBoard}>Play Again!</button>
       </div>
     </div>
