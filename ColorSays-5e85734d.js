ea0826c330ce93825a87d25087d2f4eba03240e5
writebetacode <writebetacode@users.noreply.github.com>
2021-02-08 16:17:10 +0000

    deploy: 6d1cd67e9b003951628eb32c11dd93e968630cd2

---
+++ ColorSays-5e85734d.js
@@ -0,0 +1,2 @@
+import{q as o}from"./bundle-68f62faf.js";const r=o('<div class="color-memory"></div>',2);var e=function(){return r.cloneNode(!0)};export default e;export{e as ColorSays};
+//# sourceMappingURL=ColorSays-5e85734d.js.map
