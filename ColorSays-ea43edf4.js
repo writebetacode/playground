28ea2cb43e5aa36bcfe37e99339e92900d874892
writebetacode <writebetacode@users.noreply.github.com>
2022-11-03 23:59:01 +0000

    deploy: 8497e3727245ba649823531a75254d2c441b57e0

---
+++ ColorSays-ea43edf4.js
@@ -0,0 +1,2 @@
+import{q as s}from"./bundle-b34f1e23.js";const d=s('<div class="color-says"><div class="header"></div><div class="board"><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div></div></div>',14);var a=function(){return d.cloneNode(!0)};export{a as ColorSays,a as default};
+//# sourceMappingURL=ColorSays-ea43edf4.js.map
